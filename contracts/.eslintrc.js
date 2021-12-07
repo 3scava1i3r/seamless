f2bc8044465f9b7542c5de17e8fc649293475e5d
Hritwik Tripathi <tripathi.hritwik@gmail.com>
2021-12-08 03:36:22 +0530

    added sc

--- contracts/.eslintrc.js
+++ contracts/.eslintrc.js
@@ -1,32 +1,10 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    "plugin:@typescript-eslint/eslint-recommended",
-    'plugin:@typescript-eslint/recommended',
-  ],
-  env: {
-    mocha: true,
-    node: true,
-  },
-  rules: {
-    'indent': [
-      'error',
-      2,
-    ],
-    'comma-dangle': [
-      'error',
-      'always-multiline',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "parser": "babel-eslint"
 }
+
--- contracts/contracts/IRegistry.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.6.2;
-
-
-interface IRegistry {
-    function isValid(bytes32 item) external view returns (bool);
-}
